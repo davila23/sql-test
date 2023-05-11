1cf7bd7aafda3239a2deceecc1eee056ab0476b0
davila23 <daniel.avila@rottay.com>
2023-05-11 08:53:25 -0400

    Update excercise 3 and 5

--- src/exercise-5/index.sql
+++ src/exercise-5/index.sql
@@ -2,8 +2,14 @@
 
 SELECT e1.SensorId, e1.EventType, e1.Value - e2.Value AS DeltaValue
 FROM Events e1
-INNER JOIN Events e2 ON e1.SensorId = e2.SensorId AND e1.EventType = e2.EventType AND e1.Time = (
-    SELECT MAX(Time)
-    FROM Events
-    WHERE SensorId = e1.SensorId AND EventType = e1.EventType AND Time < e1.Time AND Time >= DATEADD(hour, -24, GETDATE())
-) AND e2.Time >= DATEADD(hour, -24, GETDATE());
+INNER JOIN Events e2 ON e1.SensorId = e2.SensorId 
+                      AND e1.EventType = e2.EventType 
+                      AND e1.Time > e2.Time
+LEFT JOIN Events e3 ON e1.SensorId = e3.SensorId 
+                      AND e1.EventType = e3.EventType 
+                      AND e3.Time > e2.Time 
+                      AND e3.Time < e1.Time
+WHERE e3.SensorId IS NULL
+AND e1.Value > e2.Value
+AND e1.Time >= datetime('now', '-24 hour')
+AND e2.Time >= datetime('now', '-24 hour');
